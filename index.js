f2f65cc9b46e0d6a694c168ffa57dfdd0c6cd9ea
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-03-08 09:09:09 -0500

    funcionando

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParse = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3977;
+
+app.use(bodyParse.urlencoded({ extended: true })); app.use(bodyParse.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send({ msg: "Hola Mundo" });
+});
+
+app.post("/welcome", (req, res) => {
+    const {username} = req.body;
+    res.status(200).send({ msg:'hola '+ username });
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor funcionando')
+})
